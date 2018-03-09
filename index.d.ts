ae7e130e94f05f038b107f40910ca9e58599fb3b
Apostrophii <luke.foobar@gmail.com>
2018-03-08 16:39:49 -0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,13 @@
-// PR welcome
+declare module "react-progressive-bg-image"  {
+  export interface ProgressiveBgImageProps {
+    src: string;
+    placeholder: string;
+    opacity?: number;
+    blur?: number;
+    scale?: number;
+    transition?: string;
+    component?: string;
+  }
+
+  export default class ProgressiveBgImage extends React.Component<ProgressiveBgImageProps>{}
+}
